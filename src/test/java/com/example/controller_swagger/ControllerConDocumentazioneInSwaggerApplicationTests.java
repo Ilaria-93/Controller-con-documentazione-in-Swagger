c94763a7ecf7dbf05bfde6ff81ae772b8020ff0a
Ilaria Faleschini <ilaria.faleschini@gmail.com>
2024-07-04 11:53:12 +0200

    first commit

---
+++ src/test/java/com/example/controller_swagger/ControllerConDocumentazioneInSwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controller_swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerConDocumentazioneInSwaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
